1c2803b219709c227f5ec111df29e9f95fc4b319
Tod Burtchell <tod.burtchell@netfoundry.io>
2021-10-26 09:04:30 -0400

    Oracle shim - remove NIO provider, adding oracle.jdbc.javaNetNio=false

--- src/main/java/org/openziti/jdbc/shim/Oracle.java
+++ src/main/java/org/openziti/jdbc/shim/Oracle.java
@@ -1,14 +1,19 @@
 package org.openziti.jdbc.shim;
 
-import static org.openziti.jdbc.ZitiDriver.ZitiFeature.nioProvider;
+import static org.openziti.jdbc.ZitiDriver.ZitiFeature.seamless;
 import java.util.EnumSet;
+import java.util.Properties;
+
 import org.openziti.jdbc.BaseZitiDriverShim;
 
 public class Oracle extends BaseZitiDriverShim {
 
-  public Oracle() throws ReflectiveOperationException {
-    super("^zdbc:oracle:thin.*", 
-        "oracle.jdbc.OracleDriver", 
-        EnumSet.of(nioProvider));
-  }
+	public Oracle() throws ReflectiveOperationException {
+		super("^zdbc:oracle:thin.*", "oracle.jdbc.OracleDriver", EnumSet.of(seamless));
+	}
+
+	@Override
+	public void configureDriverProperties(Properties props) {
+		props.setProperty("oracle.jdbc.javaNetNio", "false");
+	}
 }
